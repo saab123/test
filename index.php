15910e1a3c18e3af631493dd8fa3604ca2c8ccfc
sabri <sabri.hendrick@student.ehb.be>
2018-10-30 19:08:24 +0100

    testen van git push

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sabri
+ * Date: 30-10-18
+ * Time: 18:11
+ */
+?>
+
+
+
+
+
+<html>
+
+<head>
+    <title></title>
+</head>
+<body>
+<p>
+<li>
+
+</li>
+</p>
+</body>
+</html>
